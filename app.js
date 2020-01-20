1b865d226e214766d893f33f5e7dec0fb8b6d941
Constant Dupuis <constant.dupuis@gmail.com>
2020-01-20 11:48:01 +0100

    Create a model object And use it the app

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 // Modules
-const {app, BrowserWindow} = require('electron');
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
+const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const model = require("./model/model");
 
-const canvaPaths = [];
 let webAPI;
+
+console.log("Model version ", model.version);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    width: 1000,
+    height: 800,
     webPreferences: { nodeIntegration: true },
-    title : "Generative Art Gallery Canvas"
+    title: "Generative Art Gallery Canvas"
   });
 
-  mainWindow.setBackgroundColor("#FFFFFF")
+  mainWindow.setBackgroundColor("#FFFFFF");
 
-  fs.readdir("./canvas", (err, items) => {
-    for (const dirent of items) {
-      //console.log(dirent);
-      canvaPaths.push( dirent);
-    }
-    //console.log(canvaPaths);
-
-    if( canvaPaths.length == 0)
-    {
+  model.load(() => {
+    if (model.canvas.length == 0) {
       console.log("Load no canvas HTML warning");
-      mainWindow.loadFile('nocanvas.html');
-    }
-    else{
-      loadCanvas( 0);
+      mainWindow.loadFile("nocanvas.html");
+    } else {
+      loadCanvas(0);
     }
   });
+
+  // fs.readdir("./canvas", (err, items) => {
+  //   for (const dirent of items) {
+  //     //console.log(dirent);
+  //     canvaPaths.push( dirent);
+  //   }
+  //   //console.log(canvaPaths);
+
+  //   if( canvaPaths.length == 0)
+  //   {
+  //     console.log("Load no canvas HTML warning");
+  //     mainWindow.loadFile('nocanvas.html');
+  //   }
+  //   else{
+  //     loadCanvas( 0);
+  //   }
+  // });
 
   // set web api rounting
   setupAPI();
@@ -49,87 +60,69 @@ function createWindow () {
   mainWindow.setMenuBarVisibility(false);
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
+  mainWindow.on("closed", () => {
+    mainWindow = null;
   });
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
 });
 
 // load acanvas in main Electron window
-function loadCanvas( canvasId)
-{
-  const loadURL = `./canvas/${canvaPaths[canvasId]}/index.html`;
+function loadCanvas(canvasId) {
+  const loadURL = `./canvas/${model.canvas[canvasId].path}/index.html`;
   console.log("Load first canvas HTML " + loadURL);
   mainWindow.loadFile(loadURL);
 }
 
 // setup and start WEB API for remote control
-function setupAPI()
-{
+function setupAPI() {
   webAPI = express();
 
   const apiRoute = express.Router();
 
   // root route, return info an app
-  apiRoute.get('/', (req, res)=>{
+  apiRoute.get("/", (req, res) => {
     res.json({
-      "api":"remote control",
-      "app":"Generative Art Gallery Canvas",
-      "author":"Constant Dupuis"
+      api: "remote control",
+      app: "Generative Art Gallery Canvas",
+      author: "Constant Dupuis"
     });
   });
 
   // list available canvas
-  apiRoute.get('/canvas', (req, res)=>{
-
-    let canvasNfo = [];
-
-    canvaPaths.forEach((e,i) => {
-      let canvaNfo = {};
-      canvaNfo.id = i;
-      canvaNfo.name = e;
-      canvaNfo.desc = "nothing yet";
-      canvasNfo.push(canvaNfo);
-    });
-
-    res.json(canvasNfo);
+  apiRoute.get("/canvas", (req, res) => {
+    res.json(model.canvas);
   });
 
   // activate a given canvas
-  apiRoute.post('/canvas/:id', (req, res)=>{
-    
+  apiRoute.post("/canvas/:id", (req, res) => {
     //console.log(`Load canvas ${req.params.id}`);
-    if( req.params.id >= canvaPaths.length || req.params.id < 0)
-    {
+    if (req.params.id >= model.canvas.length || req.params.id < 0) {
       // log this issue
-      console.log(`API: Requested canvas index out of range [${req.params.id}]`);
-      res.status(400).send(`API: Requested canvas index out of range [${req.params.id}]`);  
-    }
-    else
-    {
-      loadCanvas( req.params.id);
+      console.log(
+        `API: Requested canvas index out of range [${req.params.id}]`
+      );
+      res
+        .status(400)
+        .send(`API: Requested canvas index out of range [${req.params.id}]`);
+    } else {
+      loadCanvas(req.params.id);
       res.status(200).send(`Canvas [${req.params.id}] loaded`);
     }
   });
 
-  webAPI.use('/v0.1', apiRoute);
+  webAPI.use("/v0.1", apiRoute);
 
   webAPI.listen(33366);
 }
-
-
-
-
-
---
+++ model/model.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+model = {};
+model.version = "0.1";
+model.canvas = [];
+
+model.load = function(callback) {
+  console.log("Load model");
+
+  fs.readdir("./canvas", (err, items) => {
+    console.log(items);
+    items.forEach((e, i) => {
+      let infoPath = "./canvas/" + e + "/infos.json";
+      console.log("Load canvas from", e, "info in ", infoPath);
+      let rawdata = fs.readFileSync(infoPath);
+      let canvasNfo = JSON.parse(rawdata);
+      canvasNfo.id = i;
+      canvasNfo.path = e;
+      console.log(canvasNfo);
+      this.canvas.push(canvasNfo);
+    });
+
+    callback();
+  });
+};
+
+module.exports = model;
